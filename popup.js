df40ce2b8555d5cc5d8157b619377cf268c19b06
Will Ginsberg <will.j.ginsberg@gmail.com>
2023-08-06 13:04:43 -0400

    add inital edge case to popup

--- popup.js
+++ popup.js
@@ -3,9 +3,19 @@ const ul = document.getElementById("tabs")
 chrome.storage.local.get("meta.recent")
     .then((storage) => {
         const recents = storage["meta.recent"] || []
+
+        if (recents.length === 0) {
+            const container = document.getElementById("error")
+            container.hidden = ""
+            return
+        }
+
         for(const name of recents.reverse()) {
             const li = document.createElement("li")
             li.textContent = name
             ul.appendChild(li)
-        }        
+        }
+
+        const container = document.getElementById("loaded")
+        container.hidden = ""
     })
