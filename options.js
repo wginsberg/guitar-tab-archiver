daec44e3749f088473114a490d7514ced0fe6e7e
Will Ginsberg <will.j.ginsberg@gmail.com>
2023-08-21 11:19:59 -0400

    downgrade manifest to v2 from v3

--- options.js
+++ options.js
@@ -1,98 +1,97 @@
 const ul = document.getElementById("tabs")
 
-chrome.storage.local.get()
-    .then(storage => {
-        const tabNames = Object.keys(storage)
-            .filter(name => !name.startsWith("meta."))
-            .sort()
+chrome.storage.local.get(storage => {
+    const tabNames = Object.keys(storage)
+        .filter(name => !name.startsWith("meta."))
+        .sort()
 
-        if (tabNames.length === 0) {
-            document.getElementById("loading").hidden = "hidden"
-            document.getElementById("error").hidden = ""
-            return
-        }
-
-        let currentArtistName
-        for(const name of tabNames) {
-
-            // Add artist name heading
-            const [artistName, songName] = name.split(" - ")
-            if (artistName !== currentArtistName) {
-                currentArtistName = artistName
-                const h3 = document.createElement("h5")
-                h3.textContent = artistName
-                h3.id = artistName
-                const hr = document.createElement("hr")
-                ul.append(h3)
-                ul.append(hr)
-            }
-
-            const tab = storage[name]
-
-            const li = document.createElement("li")
-            const button = document.createElement("a")
-            button.textContent = songName            
-            li.onclick = event => {
-                if (event.target.tagName === "BUTTON") return
-
-                const tabContainerDiv = document.getElementById("tab")
-                tabContainerDiv.hidden = ""
-
-                const tab_heading = document.getElementById("tab_heading")
-                tab_heading.replaceChildren()
-                {
-                    const a = document.createElement("a")
-                    a.href= `#${artistName}`
-                    a.text = artistName
-                    tab_heading.appendChild(a)
-                }
-                {
-                    const text = document.createTextNode(" - ")
-                    tab_heading.appendChild(text)
-                }
-                {
-                    const span = document.createElement("span")
-                    span.innerText = songName
-                    tab_heading.appendChild(span)
-                }
-
-                const pre = document.getElementById("pre")
-                pre.textContent = tab
-
-                setTimeout(() => tab_heading.scrollIntoView({behavior:"smooth"}), 150)
-                document.title = name
-            }
-            li.appendChild(button)
-
-            const deleteButton = document.createElement("button")
-            const deleteButtonLabel = `Delete ${name}`
-            deleteButton.title = deleteButtonLabel
-            deleteButton.ariaLabel = deleteButtonLabel
-            deleteButton.classList.add("delete")
-            deleteButton.innerText = "❌"
-            deleteButton.onclick = () => {
-                const confirmation = window.confirm(`Are you sure you want to delete ${name}?`)
-                if (!confirmation) return
-                // remove tab data
-                chrome.storage.local.remove(name)
-                    .then(() => location.reload())
-                // remove tab from "recent" list
-                chrome.storage.local.get("meta.recent")
-                    .then(storage => {
-                        const recents = storage["meta.recent"]
-                        console.log({ recents, name, includes: recents.includes(name)})
-                        if (!recents.includes(name)) return
-                        const newRecents = recents.filter(recent => recent !== name)
-                        console.log({ newRecents })
-                        chrome.storage.local.set({ "meta.recent": newRecents })
-                            .then(s => console.log("done", s))
-                    })
-            }
-
-            li.append(deleteButton)
-            ul.appendChild(li)
-        }
-
+    if (tabNames.length === 0) {
         document.getElementById("loading").hidden = "hidden"
-        document.getElementById("loaded").hidden = ""
-    })
+        document.getElementById("error").hidden = ""
+        return
+    }
+
+    let currentArtistName
+    for(const name of tabNames) {
+
+        // Add artist name heading
+        const [artistName, songName] = name.split(" - ")
+        if (artistName !== currentArtistName) {
+            currentArtistName = artistName
+            const h3 = document.createElement("h5")
+            h3.textContent = artistName
+            h3.id = artistName
+            const hr = document.createElement("hr")
+            ul.append(h3)
+            ul.append(hr)
+        }
+
+        const tab = storage[name]
+
+        const li = document.createElement("li")
+        const button = document.createElement("a")
+        button.textContent = songName            
+        li.onclick = event => {
+            if (event.target.tagName === "BUTTON") return
+
+            const tabContainerDiv = document.getElementById("tab")
+            tabContainerDiv.hidden = ""
+
+            const tab_heading = document.getElementById("tab_heading")
+            tab_heading.replaceChildren()
+            {
+                const a = document.createElement("a")
+                a.href= `#${artistName}`
+                a.text = artistName
+                tab_heading.appendChild(a)
+            }
+            {
+                const text = document.createTextNode(" - ")
+                tab_heading.appendChild(text)
+            }
+            {
+                const span = document.createElement("span")
+                span.innerText = songName
+                tab_heading.appendChild(span)
+            }
+
+            const pre = document.getElementById("pre")
+            pre.textContent = tab
+
+            setTimeout(() => tab_heading.scrollIntoView({behavior:"smooth"}), 150)
+            document.title = name
+        }
+        li.appendChild(button)
+
+        const deleteButton = document.createElement("button")
+        const deleteButtonLabel = `Delete ${name}`
+        deleteButton.title = deleteButtonLabel
+        deleteButton.ariaLabel = deleteButtonLabel
+        deleteButton.classList.add("delete")
+        deleteButton.innerText = "❌"
+        deleteButton.onclick = () => {
+            const confirmation = window.confirm(`Are you sure you want to delete ${name}?`)
+            if (!confirmation) return
+            // remove tab data
+            chrome.storage.local.remove(name)
+                .then(() => location.reload())
+            // remove tab from "recent" list
+            chrome.storage.local.get("meta.recent")
+                .then(storage => {
+                    const recents = storage["meta.recent"]
+                    console.log({ recents, name, includes: recents.includes(name)})
+                    if (!recents.includes(name)) return
+                    const newRecents = recents.filter(recent => recent !== name)
+                    console.log({ newRecents })
+                    chrome.storage.local.set({ "meta.recent": newRecents })
+                        .then(s => console.log("done", s))
+                })
+        }
+
+        li.append(deleteButton)
+        ul.appendChild(li)
+    }
+
+    document.getElementById("loading").hidden = "hidden"
+    document.getElementById("loaded").hidden = ""
+})
--- popup.js
+++ popup.js
@@ -1,21 +1,20 @@
 const ul = document.getElementById("tabs")
 
-chrome.storage.local.get("meta.recent")
-    .then((storage) => {
-        const recents = storage["meta.recent"] || []
+chrome.storage.local.get("meta.recent", (storage) => {
+    const recents = storage["meta.recent"] || []
 
-        if (recents.length === 0) {
-            const container = document.getElementById("error")
-            container.hidden = ""
-            return
-        }
-
-        for(const name of recents.reverse()) {
-            const li = document.createElement("li")
-            li.textContent = name
-            ul.appendChild(li)
-        }
-
-        const container = document.getElementById("loaded")
+    if (recents.length === 0) {
+        const container = document.getElementById("error")
         container.hidden = ""
-    })
+        return
+    }
+
+    for(const name of recents.reverse()) {
+        const li = document.createElement("li")
+        li.textContent = name
+        ul.appendChild(li)
+    }
+
+    const container = document.getElementById("loaded")
+    container.hidden = ""
+})
