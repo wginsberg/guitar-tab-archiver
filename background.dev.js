ed39aee801297929e132a7bc83113db281cd7119
Will Ginsberg <will.j.ginsberg@gmail.com>
2023-09-10 15:17:21 -0400

    add padding to main

--- background.dev.js
+++ background.dev.js
@@ -1,5 +1,13 @@
 import { addNewTab } from "./background.prod.js"
 
+const tabName = "Test Artist - TEST CHORDS"
+const tabContent = `
+    Test content
+
+    A long line:
+    ----------------------------------------------------------------------------------------
+`
+
 browser.runtime.onInstalled.addListener(() => {
-    addNewTab("Test Artist - TEST CHORDS", "test content")
+    addNewTab(tabName, tabContent)
 });
