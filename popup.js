6f0ffc96ee436387ac542f627fba79e80f3f436d
Will Ginsberg <will.j.ginsberg@gmail.com>
2023-11-09 16:12:57 -0500

    update popup styling

--- popup.js
+++ popup.js
@@ -12,17 +12,23 @@ browser.runtime.sendMessage({ type: "GET_RECENTS"})
             const [artist, song] = name.split(" - ")
             const li = document.createElement("li")
 
-            const strong = document.createElement("strong")
-            strong.className = "artist"
-            strong.textContent = artist
-
             const a = document.createElement("a")
             a.href = `options.html?name=${encodeURIComponent(name)}`
             a.target="_blank"
-            a.textContent = song
-
-            li.append(strong)
             li.append(a)
+
+            const strong = document.createElement("strong")
+            strong.className = "artist"
+            strong.textContent = artist
+            a.append(strong)
+
+            const br = document.createElement("br")
+            a.append(br)
+
+            const span = document.createElement("span")
+            span.className = "song"
+            span.textContent = song
+            a.append(span)
 
             ul.appendChild(li)
         }
