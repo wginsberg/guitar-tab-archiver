6a0669c4fd4376ab23b8d484acba6d7f09f77592
Will Ginsberg <will.j.ginsberg@gmail.com>
2023-11-23 17:40:07 -0500

    fix delete

--- src/background/messages/gta.ts
+++ src/background/messages/gta.ts
@@ -2,15 +2,12 @@ import type { PlasmoMessaging } from "@plasmohq/messaging"
 import type { GTAMessageType, GTAMessage, GTAMessageResult } from "~types/messages"
 import { addNewTab, getRecents, getAll, getTab, deleteTab, deleteAllTabs } from "~background/background"
 
-
-console.log("env = = ", process.env["NODE_ENV"])
-
 const handler: PlasmoMessaging.Handler<GTAMessageType, GTAMessage, GTAMessageResult> = async (req, res) => {
-    console.log({req})
     switch (req.body.type) {
         case "ADD": {
             const { tabName, tabContent} = req.body
             await addNewTab(tabName, tabContent)
+            break
         }
         case "GET_RECENTS": {
             const recents = await getRecents()
@@ -38,7 +35,11 @@ const handler: PlasmoMessaging.Handler<GTAMessageType, GTAMessage, GTAMessageRes
             await deleteAllTabs()
             break
         }
+        default: {
+            throw new Error(`Unsupported message: ${req.name}`)
+        }
     }
+    res.send("ok")
 }
  
 export default handler
