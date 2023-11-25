a0035f5e2f5cf3ad0dbb3aef88dea9bc5a590b9a
Will Ginsberg <will.j.ginsberg@gmail.com>
2023-11-25 11:46:44 -0500

    disable autocomplete on search form

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -4,7 +4,12 @@ const SearchForm: React.FC = () => {
     return (
         <form className="center search" target="_blank" rel="noopener noreferrer" action="https://ultimate-guitar.com/search.php">
             <input hidden readOnly name="search_type" value="title"></input>
-            <input type="text" placeholder="Search ultimate-guitar.com" name="value" />
+            <input
+                type="text"
+                placeholder="Search ultimate-guitar.com"
+                name="value"
+                autoComplete="off"
+            />
             <button>Search</button>
         </form>
     )
